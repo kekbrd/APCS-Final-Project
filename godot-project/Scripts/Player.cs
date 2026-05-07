225525c6f147f142e81f82a14f8afc5af2dbe703
kekbrd <atnekemails@gmail.com>
2026-05-06 17:43:24 -0700

    Working movement and some tiles

--- godot-project/Scripts/Player.cs
+++ godot-project/Scripts/Player.cs
@@ -4,17 +4,14 @@ using System.Runtime.ConstrainedExecution;
 
 public partial class Player : CharacterBody2D
 {
-	private const float Speed = 200.0f;
+	private const float speed = 200;
 	// Called when the node enters the scene tree for the first time.
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta) // PhysicsProcess is used for movement, not Process.
 	{
-		Vector2 velocity = Vector2.Zero;
-		velocity.X = Input.GetAxis("move_left", "move_right");
-		velocity.Y = Input.GetAxis("move_up", "move_down");
-		Velocity = velocity.Normalized() * Speed;
-		GD.Print(Velocity);
+		Velocity = Vector2.Zero;
+		Velocity = Input.GetVector("move_left", "move_right", "move_up", "move_down") * speed; // Got this from AI. Up is negative y, down is positive.
 		MoveAndSlide();
 	}
 }
