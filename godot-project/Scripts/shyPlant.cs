3a484871610aaac73b556016c825cda23dd249dc
kekbrd <atnekemails@gmail.com>
2026-05-08 15:21:06 -0700

    some script work

--- godot-project/Scripts/shyPlant.cs
+++ godot-project/Scripts/shyPlant.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Threading;
 
 public partial class shyPlant : CharacterBody2D
 {
@@ -7,7 +8,9 @@ public partial class shyPlant : CharacterBody2D
 	private const float walkSpeed = 40;
 	private const float runSpeed = 200;
 	private Boolean playerInRange = false;
-	private void body_entered(Node2D body) // Method heater written by AI because even though I told it not to show me code it still did.
+	private Boolean isFleeing = false;
+	private Boolean isWalking = false;
+    private void body_entered(Node2D body) // Method heater written by AI because even though I told it not to show me code it still did.
 	{
 		if (body is Player) // == and .equals don't work in C#
 		{
@@ -21,14 +24,69 @@ public partial class shyPlant : CharacterBody2D
 			playerInRange = false;
 		}
 	}
+
+	private void on_timer_timeout()
+	{
+        setWalkVelocity();
+        GetTree().CreateTimer(2);
+        Random r = new Random();
+        Timer.Start(r.Next(1, 7));
+	}
+
+    public override void _Ready()
+    {
+		setRandomDirection(); // C# doesn't let you write just method name
+    }
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta) // Apparently if statements have to be in methods
 	{
-		Velocity = new Vector2(1, 1);
-		if (playerInRange)
+		if (!playerInRange)
 		{
 			Velocity *= runSpeed;
 		}
 		MoveAndSlide();
 	}
+
+    private void setRandomDirection()
+    {
+        Random r = new Random();
+        int i = r.Next(0, 4); // inclusive and exclusive
+        if (i == 0)
+        {
+            cardinalDirection = 'N';
+        }
+        else if (i == 1)
+        {
+            cardinalDirection = 'E';
+        }
+        else if (i == 2)
+        {
+            cardinalDirection = 'S';
+        }
+        else
+        {
+            cardinalDirection = 'W';
+        }
+    }
+
+	private void setWalkVelocity()
+	{
+        if (cardinalDirection == 'N')
+        {
+            Velocity = new Vector2(0, -1);
+        }
+        else if (cardinalDirection == 'E')
+        {
+            Velocity = new Vector2(1, 0);
+        }
+        else if (cardinalDirection == 'S')
+        {
+            Velocity = new Vector2(0, 1);
+        }
+        else
+        {
+            Velocity = new Vector2(-1, 0);
+        }
+    }
 }
